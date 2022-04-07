197ce890a284283ad05eda10f84003173ffe6a96
mexmi2021 <30824242+mexmi2021@users.noreply.github.com>
2022-04-07 14:09:29 +0800

    Update model_scheduler.py

--- mexmi/utils/model_scheduler.py
+++ mexmi/utils/model_scheduler.py
@@ -69,7 +69,7 @@ def train_step_vmi(model, train_loader, train_gt_loader=None, criterion=None, op
     train_loss_batch = 0
     epoch_size = len(train_loader.dataset)
     t_start = time.time()
-    #一个训练过程
+    #
     i=0
     for batch_idx, (inputs, targets) in enumerate(train_loader):
         inputs, targets = inputs.to(device), targets.to(device)
@@ -131,7 +131,7 @@ def train_step(model, train_loader, train_gt_loader=None, criterion=None, optimi
     train_loss_batch = 0
     epoch_size = len(train_loader.dataset)
     t_start = time.time()
-    #一个训练过程
+    #
     i=0
     for batch_idx, (inputs, targets) in enumerate(train_loader):
         inputs, targets = inputs.to(device), targets.to(device)
@@ -219,7 +219,7 @@ def test_step(model, test_loader, criterion, device, epoch=0., blackbox=None, si
                 fid_num5 += fid_num5_t[:5].reshape(-1).float().sum(0, keepdim=True)
                 fid_num += predicted.eq(true_label).sum().item()
 
-            if batch_idx >= 1249: #只选取10000个做test
+            if batch_idx >= 1249: #
                 break
 
     t_end = time.time()
@@ -375,7 +375,7 @@ def train_model(model, trainset, trainset_gt=None, out_path=None, blackbox=None,
                                           )
 
     #How
-    if weighted_loss:#loss 是有权重的
+    if weighted_loss:#loss
         if not isinstance(trainset.samples[0][1], int):
             print('Labels in trainset is of type: {}. Expected: {}.'.format(type(trainset.samples[0][1]), int))
 
@@ -402,7 +402,7 @@ def train_model(model, trainset, trainset_gt=None, out_path=None, blackbox=None,
     start_epoch = 1
     best_train_acc, train_acc = -1., -1.
     best_test_acc, test_acc, test_loss, best_fidelity= -1., -1., -1., -1.
-    # Resume if required 从某个模型继续训练
+    # Resume if required
     if resume is not None:
         model_path = resume
         if osp.isfile(model_path):
@@ -431,7 +431,7 @@ def train_model(model, trainset, trainset_gt=None, out_path=None, blackbox=None,
     # model_out_path = osp.join(out_path, '{}_50000.pth.tar'.format(s_m))
     model_out_path = osp.join(out_path, 'checkpoint_{}.pth.tar'.format(checkpoint_suffix))
     for epoch in range(start_epoch, epochs + 1): #1，101
-        #在这里就跑完了一个epoch
+        #
         if imp_vic_mem:
             train_loss, train_acc = train_step_vmi(model, train_loader, train_gt_loader, criterion_train, optimizer, epoch,
                                                device, log_interval,
